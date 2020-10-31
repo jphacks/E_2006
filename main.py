52d03632d138ab0c45604d52d97ed3140a5fe336
Masahisa Honda <masahisa0910@gmail.com>
2020-10-31 11:41:49 +0900

    opencvを用いたカメラの起動

---
+++ main.py
@@ -0,0 +1,17 @@
+import cv2
+cap = cv2.VideoCapture(0)
+while True:
+    # 1フレームずつ取得する。
+    ret, frame = cap.read()
+    # フレームが取得できなかった場合は、画面を閉じる
+    if not ret:
+        break
+
+    # ウィンドウに出力
+    cv2.imshow("カメラアプリ", frame)
+    key = cv2.waitKey(1)
+    # Escキーを入力されたら画面を閉じる
+    if key == 27 or key == ord("q"):
+        break
+cap.release()
+cv2.destroyAllWindows()
