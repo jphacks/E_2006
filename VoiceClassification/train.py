b3a24fbddac4da000e427147e122dd70f1eb01dd
KohheiAdachi <d13301@yonago.kosen-ac.jp>
2020-11-03 22:21:11 +0900

    voiceClassification

---
+++ VoiceClassification/train.py
@@ -0,0 +1,10 @@
+import numpy as np
+import soundfile as sf
+import pyaudio
+import wave
+
+import recorder
+
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split, cross_val_score
+
